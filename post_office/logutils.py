08205e993a69dd115c26906d6d2e5143579b06c7
pix <pix666@ya.ru>
2019-01-10 13:25:48 +0400

    Prevent force logging
    (cherry picked from commit 5a0daefc313cda2d2ec31d84cbce490025661644)
    
    # Conflicts:
    #	post_office/logutils.py

--- post_office/logutils.py
+++ post_office/logutils.py
@@ -1,36 +1,32 @@
 import logging
-from logging.config import dictConfig
 
 
 # Taken from https://github.com/nvie/rq/blob/master/rq/logutils.py
 def setup_loghandlers(level=None):
     # Setup logging for post_office if not already configured
     logger = logging.getLogger('post_office')
-    if not logger.handlers:
-        dictConfig({
-            "version": 1,
-            "disable_existing_loggers": False,
-
-            "formatters": {
-                "post_office": {
-                    "format": "[%(levelname)s]%(asctime)s PID %(process)d: %(message)s",
-                    "datefmt": "%Y-%m-%d %H:%M:%S",
-                },
-            },
-
-            "handlers": {
-                "post_office": {
-                    "level": "DEBUG",
-                    "class": "logging.StreamHandler",
-                    "formatter": "post_office"
-                },
-            },
-
-            "loggers": {
-                "post_office": {
-                    "handlers": ["post_office"],
-                    "level": level or "DEBUG"
-                }
-            }
-        })
+    if not _has_effective_handler(logger):
+        logger.setLevel(level)
+        formatter = logging.Formatter(
+            fmt='[%(levelname)s]%(asctime)s PID %(process)d: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
+
+
+def _has_effective_handler(logger):
+    """
+    Checks if a logger has a handler that will catch its messages in its logger hierarchy.
+    :param `logging.Logger` logger: The logger to be checked.
+    :return: True if a handler is found for the logger, False otherwise.
+    :rtype: bool
+    """
+    while True:
+        if logger.handlers:
+            return True
+        if not logger.parent:
+            return False
+        logger = logger.parent
